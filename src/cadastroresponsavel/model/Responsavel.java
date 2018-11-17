fc93d212f2f93bd4a4db77dc746c6472f3bd39a7
Laura Radicchi <>
2018-11-17 21:14:00 -0200

    primeiro commit

---
+++ src/cadastroresponsavel/model/Responsavel.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+
+public class Responsavel{
+    public Responsavel(){
+
+    }
+    private String nomeResponsavel;
+    private String telefoneResponsavel;
+
+    public String getTelefoneResponsavel(){
+      return telefoneResponsavel;
+    }
+    public void setTelefoneResponsavel(String telefoneResponsavel){
+      this.telefoneResponsavel=telefoneResponsavel;
+    }
+    public String getNomeResponsavel(){
+      return nomeResponsavel;
+    }
+    public void setNomeResponsável(){
+      
+    }
+}
