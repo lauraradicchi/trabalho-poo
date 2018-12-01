f519bb5300fecef8597dcdeeebe3dc871a83964b
Laura Radicchi <lauramradicchi@gmail.com>
2018-12-01 00:17:13 -0200

    criada as classes para a implementação do aluno

---
+++ src/cadastroresponsavel/model/Aluno.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastroresponsavel.model;
+
+/**
+ *
+ * @author Laura
+ */
+public class Aluno {
+    private String nome;
+    private String dataNascimento;
+    private String telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+}
